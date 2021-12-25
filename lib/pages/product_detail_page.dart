8a664f27605c199274b10a6ed37a4691c1320668
Tjard Lüdeke <tjard.luedeke@rellingen.info>
2021-12-25 17:05:52 +0100

    after detail page

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -1,30 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:iieproject/styles.dart';
 
 class ProductDetailPage extends StatefulWidget {
-  const ProductDetailPage({ Key? key, required this.images }) : super(key: key);
-final List<String> images;
+  const ProductDetailPage({Key? key, required this.images}) : super(key: key);
+  final List<String> images;
   @override
   _ProductDetailPageState createState() => _ProductDetailPageState();
 }
 
-class _ProductDetailPageState extends State<ProductDetailPage> with TickerProviderStateMixin{
+class _ProductDetailPageState extends State<ProductDetailPage>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      _buildImageCarousel(),
-      // _buildDescription(),
-      // _buildBottomBar()
-    ],
-      
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(
+            Icons.close,
+            color: Colors.black87,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(
+              Icons.ios_share_rounded,
+              color: Colors.black87,
+            ),
+          ),
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(
+              Icons.favorite_border_rounded,
+              color: Colors.black87,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 0),
+              children: [
+                _buildImageCarousel(),
+                _buildDescription(),
+              ],
+            ),
+          ),
+          _buildBottomBar()
+        ],
+      ),
     );
   }
 
-  _buildImageCarousel(){
-     TabController imagesController =
+  _buildDescription() {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Professional Guitar",
+            style: TextStyles.h1,
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              Icon(
+                Icons.star,
+                color: Theme.of(context).primaryColor,
+              ),
+              Text(
+                "  4,5 / 5 (100 Reviews) • Enschede, Netherlands",
+                style: TextStyles.body,
+              )
+            ],
+          ),
+          SizedBox(height: 8),
+          Divider(),
+          SizedBox(height: 8),
+          Text(
+            "Description",
+            style: TextStyles.h4,
+          ),
+          SizedBox(height: 8),
+          Text(
+            "Professional Guitar in perfect condition. Barely\nused because I have too many instruments.",
+            style: TextStyles.body,
+          ),
+          SizedBox(height: 8),
+          Divider(),
+          SizedBox(height: 24),
+          ListTile(
+            leading: Icon(
+              Icons.sell_outlined,
+            ),
+            title: Text(
+              "Open to sell",
+              style: TextStyles.h4,
+            ),
+            subtitle: Text(
+              "You can also buy this instrument",
+              style: TextStyles.body,
+            ),
+          ),SizedBox(height: 8),
+          ListTile(
+            leading: Icon(
+              Icons.calendar_today,
+            ),
+            title: Text(
+              "Flexible date",
+              style: TextStyles.h4,
+            ),
+            subtitle: Text(
+              "Extend the rent anytime",
+              style: TextStyles.body,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildBottomBar() {
+    return Material(
+      elevation: 3,
+      child: Container(
+        height: 100,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: '37€ ',
+                      style: TextStyles.h4,
+                      children: <TextSpan>[
+                        TextSpan(text: '/ week', style: TextStyles.body),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "4 Jan - 11 Jan 2022",
+                    style: TextStyles.link,
+                  )
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Container(
+                    height: 50,
+                    width: 80,
+                    child: Center(
+                        child: Text(
+                      "Reserve",
+                      style: TextStyle(fontSize: 20),
+                    )),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildImageCarousel() {
+    TabController imagesController =
         TabController(length: widget.images.length, vsync: this);
 
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(0.0),
       child: Container(
         height: 350.0,
         child: Center(
@@ -36,53 +193,15 @@ class _ProductDetailPageState extends State<ProductDetailPage> with TickerProvid
                   controller: imagesController,
                   children: widget.images.map(
                     (image) {
-                      return 
-                        Image.asset(image);
-                      
+                      return Image.asset(image);
                     },
                   ).toList(),
                 ),
-                // Align(
-                //   alignment: Alignment.bottomLeft,
-                //   child: FloatingActionButton(
-                //     elevation: 0,
-                //     backgroundColor: Colors.white.withAlpha(0),
-                //     //foregroundColor: Theme.of(context).accentColor,
-                //     child: Image.asset("images/bee.png",height: 32,),
-                //     onPressed: () {
-                //       Navigator.push(
-                //           context,
-                //           MaterialPageRoute(
-                //             builder: (context) => ZoomedImage(
-                //                 photos: widget.product
-                //                     .images[imagesController.index].imageURL),
-                //           ));
-                //     },
-                //   ),
-                // ),
-                // Align(
-                //   alignment: Alignment.bottomRight,
-                //   child: FloatingActionButton(
-                //     elevation: 0,
-                //     backgroundColor: Colors.white.withAlpha(0),
-                //     //foregroundColor: Theme.of(context).accentColor,
-                //     child: Icon(Icons.zoom_in),
-                //     onPressed: () {
-                //       Navigator.push(
-                //           context,
-                //           MaterialPageRoute(
-                //             builder: (context) => ZoomedImage(
-                //                 photos: widget.product
-                //                     .images[imagesController.index].imageURL),
-                //           ));
-                //     },
-                //   ),
-                // ),
                 Container(
                   alignment: FractionalOffset(0.5, 0.95),
                   child: TabPageSelector(
                     controller: imagesController,
-                    selectedColor: Colors.grey,
+                    selectedColor: Theme.of(context).primaryColor,
                     color: Colors.white,
                   ),
                 )
